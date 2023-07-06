89037f7b71d38636bf9f34395bae9d4a41112c77
AlSharp <elistaphilly@gmail.com>
2023-07-05 23:52:27 -0400

    add cancel and completed pages

---
+++ pages/donation/cancel/index.jsx
@@ -0,0 +1,33 @@
+import Image from 'next/future/image';
+import Layout from "components/layout/Layout";
+import siteConfig from 'config/site.config';
+import Link from 'next/link';
+
+export default function DonationCompleted() {
+  return (
+    <Layout metaTitle="Donation Canceled">
+      <div className="flex justify-center items-center max-w-screen-xl mx-auto lg:px-8">
+      <div className="flex flex-col items-center mt-10">
+          <div className="flex justify-center">
+            <Image
+              className="object-cover object-center transition duration-300 ease-in-out rounded-2xl group-hover:scale-110 w-3/4 md:w-full"
+              src={siteConfig.logoFull}
+              alt={siteConfig.logoText}
+              width={487}
+              height={618}
+              loading='eager'
+            />
+          </div>
+          <div className="mission mt-4 px-8 mb-6">
+            <h1 className="text-xl font-semibold text-center mb-3">
+              Your donation has been canceled!
+            </h1>
+          </div>
+          <div>
+            <Link href="/"><a className="text-blue-600 no-underline hover:underline">Return back to Home Page</a></Link>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
