a5ea8e5d32301018487544ddf0de229bd3253ddd
AlSharp <elistaphilly@gmail.com>
2023-07-08 00:04:55 -0400

    add statistics/donors page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Image from 'next/future/image';
+import Link from 'next/link';
 import Layout from 'components/layout/Layout';
 import siteConfig from '../config/site.config';
 
@@ -10,7 +11,7 @@ export default function Home() {
           <p className="w-full mb-3">
             The website is under active development by FKF WebDev Club. If you are interested to join the development team, please let us know via email: <a className="no-underline hover:underline" href="email:board@freekalmykia.org">board@freekalmykia.org</a> 
           </p>
-          <div className="flex flex-col md:flex-row md:items-center md:justify-center mb-2">
+          <div className="flex flex-col md:flex-row md:items-center md:justify-center mb-3">
             <div className="mb-4 md:mb-0 md:mr-8">
               <a id="first-paypal-btn" href="https://www.paypal.com/donate/?hosted_button_id=NYS2YALUACACG" target="_self" className="text-white bg-[#0070ba] hover:brightness-95 focus:ring-4 focus:ring-[#F7BE38]/50 font-medium rounded-full text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50">
                 <svg className="text-white mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="paypal" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M111.4 295.9c-3.5 19.2-17.4 108.7-21.5 134-.3 1.8-1 2.5-3 2.5H12.3c-7.6 0-13.1-6.6-12.1-13.9L58.8 46.6c1.5-9.6 10.1-16.9 20-16.9 152.3 0 165.1-3.7 204 11.4 60.1 23.3 65.6 79.5 44 140.3-21.5 62.6-72.5 89.5-140.1 90.3-43.4 .7-69.5-7-75.3 24.2zM357.1 152c-1.8-1.3-2.5-1.8-3 1.3-2 11.4-5.1 22.5-8.8 33.6-39.9 113.8-150.5 103.9-204.5 103.9-6.1 0-10.1 3.3-10.9 9.4-22.6 140.4-27.1 169.7-27.1 169.7-1 7.1 3.5 12.9 10.6 12.9h63.5c8.6 0 15.7-6.3 17.4-14.9 .7-5.4-1.1 6.1 14.4-91.3 4.6-22 14.3-19.7 29.3-19.7 71 0 126.4-28.8 142.9-112.3 6.5-34.8 4.6-71.4-23.8-92.6z"></path></svg>
@@ -21,13 +22,15 @@ export default function Home() {
               <p>Or donate with <Image className="inline-block w-10 h-5" src="/images/zelle.png" alt="Zelle" width={2500} height={1078} /> at <span className="font-bold">donate@freekalmykia.org</span></p>
             </div>
           </div>
-          <div className="md:text-center">
-            <p>Include message with your donation:</p>
+          <div className="md:text-center mb-2">
+            <p className="mb-1">Include message with your donation:</p>
             <ul className="ml-4">
-              <li>&quot;GP&quot; for General Purposes</li>
-              <li>&quot;PIM&quot; for Printing Informational Materials</li>
-              <li>&quot;KIDS&quot; for Math Class for Children</li>
+              <li>&quot;<span className="font-semibold">GP</span>&quot; for General Purposes</li>
+              <li>&quot;<span className="font-semibold">KIDS</span>&quot; for Math Class for Children</li>
             </ul>
+          </div>
+          <div className="md:text-center">
+            <Link href="/statistics/donors"><a className="text-blue-600 no-underline hover:underline font-semibold">Our donors</a></Link>
           </div>
         </div>
         <div className="absolute top-0 right-0 flex mt-2 mr-2">
---
+++ pages/statistics/donors/index.jsx
@@ -0,0 +1,72 @@
+import Image from "next/future/image";
+import Link from "next/link";
+import Layout from "components/layout/Layout";
+import siteConfig from "config/site.config";
+
+export default function Donors({ data }) {
+  const { demographics, programs, lastUpdated } = data.data;
+  return (
+    <Layout metaTitle="Statistics - Donors">
+      <div className="flex justify-content items-center max-w-screen-xl mx-auto px-8">
+        <div className="w-full py-4">
+          <div className="mb-6">
+            <Link href="/"><a className="text-blue-600 no-underline hover:underline">Return back to Home Page</a></Link>
+          </div>
+          <div className="mb-4">
+            <h1 className="text-3xl mb-4">Our donors</h1>
+            <p className="text-sm mb-4">{`Last updated: ${lastUpdated}`}</p>
+            <p className="text-xl mb-2">{ `Total: ${demographics.donors}` }</p>
+            <p className="text-base">{ `Donating monthly: ${demographics.monthly_donors}` }</p>
+            <p className="text-base">{ `Donating annually: ${demographics.annually_donors}` }</p>
+          </div>
+          <div className="mb-4">
+            <h2 className="text-xl mb-2">Geography</h2>
+            <ul>
+              {
+                Object.entries(demographics.countries).map(([country, number]) => {
+                  return (
+                    <li key={country} className="text-base">
+                      { `${country}: ${number}` }
+                      {
+                        country === 'United States'
+                        ? <ul className="ml-4 text-base">
+                            {
+                              Object.entries(demographics.states).map(([state, number]) => {
+                                return (
+                                  <li key={state}>
+                                    { `${state}: ${number}` }
+                                  </li>
+                                )
+                              })
+                            }
+                          </ul>
+                        : null
+                      }
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+          <div className="mb-4">
+            <h2 className="text-xl mb-2">Programs <span className="text-sm font-semibold">(monthly average)</span></h2>
+            <ul>
+              <li className="text-base">
+                { `General purposes: $${programs.GP.amount} receiving` }
+              </li>
+              <li className="text-bse">
+                { `Math class for children (NYC): $${programs.KIDS.amount} receiving` }
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch('http://127.0.0.1:5401/donors/v1');
+  const data = await res.json();
+  return { props: { data } };
+}
